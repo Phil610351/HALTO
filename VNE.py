04c5c4c71a15c82933e0d202822a99d548f6922c
sSplendidSs <smyonlys@gmail.com>
2020-04-16 18:45:27 +0800

    fuck

--- VNE.py
+++ VNE.py
@@ -2,20 +2,25 @@ from flask import Flask, request
 from flask_cors import CORS
 from threading import Thread
 from tkinter import *
+import json
+import requests
+import numpy as np
+import time
+headers = {'Content-Type': 'application/json'}
 app = Flask(__name__)
 cors = CORS(app, resources={r"/": {"origins": "*"}})
 @app.route('/',methods=['POST'])
-def index():
+def sockeeet():
 	req = request.get_json()
 	print(req)
-	value[req['instance']]=req['data']
+	#value[req['instance']]=req['data']
 	global route
 	route.delete("all")
-	route.create_text(200,100,text=value[0],font=('Arial', 16))
+	route.create_text(200,100,text=req['data'],font=('Arial', 16))
 	route.create_oval( 150, 50, 250, 150, width = 3 )
-	route.create_text(700,100,text=value[1],font=('Arial', 16))
+	route.create_text(700,100,text=req['data'],font=('Arial', 16))
 	route.create_oval( 650, 50, 750, 150, width = 3 )
-	route.create_text(450,500,text=value[2],font=('Arial', 16))
+	route.create_text(450,500,text=req['data'],font=('Arial', 16))
 	route.create_oval( 400, 450, 500, 550, width = 3 )
 	return 'Ok'
 def draw():
@@ -23,9 +28,35 @@ def draw():
 	root = Tk()
 	route = Canvas(root,width=900, height=600)
 	route.pack()
+	route.bind( "<Button-1>", gen_task)
 	root.mainloop()
+
+def gen_task(event):
+	print('ya')
+	load=[0]*3
+	task=list()
+	for i in range(np.random.randint(2,6)):
+		task.append(np.random.poisson(20))
+	#first fit
+	task.sort()
+	index=0
+	for e in task:
+		while 1:
+			if index>2:
+				index=0
+			if load[index]<100:
+				load[index]+=e
+				index+=1
+				break
+			else:
+				index+=1
+	print(sum(task))
+	r=requests.request('POST','http://127.0.0.1:12345', headers=headers, data=json.dumps({'data':task[0]}))
+	r=requests.request('POST','http://127.0.0.1:12345', headers=headers, data=json.dumps({'data':task[1]}))
+	r=requests.request('POST','http://127.0.0.1:12345', headers=headers, data=json.dumps({'data':task[2]}))
+
 value=['0']*3
 if __name__ == '__main__':
-	Thread(target = draw).start()
+	Thread(target=draw).start()
 	app.debug = True
 	app.run()
--- cpu.py
+++ cpu.py
@@ -1,12 +1,26 @@
+from flask import Flask, request
+from flask_cors import CORS
 import requests
 import json
 import os
 import numpy as np
 import time
 headers = {'Content-Type': 'application/json'}
-while 1:
-	os.system("sar 1 1 >output.txt")
-	with open("output.txt") as f:
-		cpu_data={'instance':0,'data':'0.'+str(np.random.poisson(10))}
-		print(cpu_data)
-		r=requests.request('POST','http://127.0.0.1:5000', headers=headers, data=json.dumps(cpu_data))	
+app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": "*"}})
+@app.route('/',methods=['POST'])
+def sockeeet():
+	req=request.get_json()
+	print(req)
+	os.system("stress-ng -c 0 -l"+str(req['data']))	
+	i=0
+	while i<5:
+		os.system("sar 1 1 >output.txt")
+		with open("output.txt") as f:
+			cpu_data={'instance':0,'data':'0.'+str(np.random.poisson(10))}
+			print(cpu_data)
+			r=requests.request('POST','http://127.0.0.1:5000', headers=headers, data=json.dumps(cpu_data))
+		i+=1
+if __name__ == '__main__':
+	app.debug = True
+	app.run(port=12345)
