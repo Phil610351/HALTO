b2c495f4422b7fd80e98e2a5630af8ce21d46241
sSplendidSs <smyonlys@gmail.com>
2020-04-16 18:48:58 +0800

    fuck

--- cpu.py
+++ cpu.py
@@ -17,7 +17,7 @@ def sockeeet():
 	while i<5:
 		os.system("sar 1 1 >output.txt")
 		with open("output.txt") as f:
-			cpu_data={'instance':0,'data':'0.'+str(np.random.poisson(10))}
+			cpu_data={'data':f.read()[-55:51]}
 			print(cpu_data)
 			r=requests.request('POST','http://127.0.0.1:5000', headers=headers, data=json.dumps(cpu_data))
 		i+=1
