afb4c2206c41b146878b2db6a27993f8eac0a3ca
Sublimenu <ssmyonlyss@egmail.com>
2020-05-27 18:43:45 +0800

    123456

--- VNE.py
+++ VNE.py
@@ -8,31 +8,46 @@ import time
 headers = {'Content-Type': 'application/json'}
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/')
 def sockeeet():
 	req = request.get_json()
 	print(req)
 	value[req['instance']]=req['data']
 	global route
-	global a
-	global b
-	global c
+	global c1
+	global c2
+	global c3
+	global c4
+	global c5
+	global c6
 	if req['instance']==0:
-		route.delete(a)
-		a=route.create_text(100,100,text=req['data'],font=('Arial', 16))
+		route.delete(c1)
+		c1=route.create_text(100,300,text=req['data'],font=('Arial', 16))
 	elif req['instance']==1:
-		route.delete(b)
-		b=route.create_text(600,100,text=req['data'],font=('Arial', 16))
+		route.delete(c2)
+		c2=route.create_text(250,100,text=req['data'],font=('Arial', 16))
 	elif req['instance']==2:
-		route.delete(c)
-		c=route.create_text(350,500,text=req['data'],font=('Arial', 16))
+		route.delete(c3)
+		c3=route.create_text(400,500,text=req['data'],font=('Arial', 16))
+	elif req['instance']==3:
+		route.delete(c4)
+		c4=route.create_text(550,500,text=req['data'],font=('Arial', 16))
+	elif req['instance']==4:
+		route.delete(c5)
+		c5=route.create_text(700,500,text=req['data'],font=('Arial', 16))
+	elif req['instance']==5:
+		route.delete(c6)
+		c6=route.create_text(850,500,text=req['data'],font=('Arial', 16))
 	else:
 		route.delete(a)
 		route.delete(b)
 		route.delete(c)			
-		a=route.create_text(100,100,text='0',font=('Arial', 16))
-		b=route.create_text(600,100,text='0',font=('Arial', 16))
-		c=route.create_text(350,500,text='0',font=('Arial', 16))
+		c1=route.create_text(100,300,text='0',font=('Arial', 16))
+		c2=route.create_text(250,300,text='0',font=('Arial', 16))
+		c3=route.create_text(400,300,text='0',font=('Arial', 16))
+		c4=route.create_text(550,300,text='0',font=('Arial', 16))
+		c5=route.create_text(700,300,text='0',font=('Arial', 16))
+		c6=route.create_text(850,300,text='0',font=('Arial', 16))
 		global d
 		for e in d:
 			route.delete(e)
@@ -42,18 +57,50 @@ def sockeeet():
 def draw():
 	global route
 	root = Tk()
-	route = Canvas(root,width=1000, height=600)
+	route = Canvas(root,width=950, height=700)
 	route.pack()
 	route.bind( "<Button-1>", gen_task)
-	global a
-	global b
-	global c
-	a=route.create_text(100,100,text='0',font=('Arial', 16))
-	route.create_oval( 50, 50, 150, 150, width = 3 )
-	b=route.create_text(600,100,text='0',font=('Arial', 16))
-	route.create_oval( 550, 50, 650, 150, width = 3 )
-	c=route.create_text(350,500,text='0',font=('Arial', 16))
-	route.create_oval( 300, 450, 400, 550, width = 3 )
+	global c1
+	global c2
+	global c3
+	global c4
+	global c5
+	global c6
+	route.create_oval( 425, 50, 525, 150, width = 3 )
+	route.create_text(475,100,text='Control',font=('Arial', 16))
+	route.create_line(475, 150, 100, 250, fill='green', width= 3 )
+	route.create_line(475, 150, 250, 250, fill='green', width= 3 )
+	route.create_line(475, 150, 400, 250, fill='green', width= 3 )
+	route.create_line(475, 150, 550, 250, fill='green', width= 3 )
+	route.create_line(475, 150, 700, 250, fill='green', width= 3 )
+	route.create_line(475, 150, 850, 250, fill='green', width= 3 )
+
+	route.create_line(150, 300, 200, 300, fill='purple', width= 3 )
+	route.create_line(300, 300, 350, 300, fill='purple', width= 3 )
+	route.create_line(450, 300, 500, 300, fill='purple', width= 3 )
+	route.create_line(600, 300, 650, 300, fill='purple', width= 3 )
+	route.create_line(750, 300, 800, 300, fill='purple', width= 3 )
+
+	c1=route.create_text(100,300,text='0',font=('Arial', 16))
+	route.create_oval( 50, 250, 150, 350, width = 3 )
+	route.create_text(100,370,text='Node1',font=('Arial', 16))
+	c2=route.create_text(250,300,text='0',font=('Arial', 16))
+	route.create_oval( 200, 250, 300, 350, width = 3 )
+	route.create_text(250,370,text='Node2',font=('Arial', 16))
+	c3=route.create_text(400,300,text='0',font=('Arial', 16))
+	route.create_oval( 350, 250, 450, 350, width = 3 )
+	route.create_text(400,370,text='Node3',font=('Arial', 16))
+	c4=route.create_text(550,300,text='0',font=('Arial', 16))
+	route.create_oval( 500, 250, 600, 350, width = 3 )
+	route.create_text(550,370,text='Node4',font=('Arial', 16))
+	c5=route.create_text(700,300,text='0',font=('Arial', 16))
+	route.create_oval( 650, 250, 750, 350, width = 3 )
+	route.create_text(700,370,text='Node5',font=('Arial', 16))
+	c6=route.create_text(850,300,text='0',font=('Arial', 16))
+	route.create_oval( 800, 250, 900, 350, width = 3 )
+	route.create_text(850,370,text='Node6',font=('Arial', 16))
+	
+	route.create_line(0, 400, 950, 400, width= 5 )
 	root.mainloop()
 
 def gen_task(event):
@@ -61,28 +108,41 @@ def gen_task(event):
 	for e in d:
 		route.delete(e)
 	d=list()
-	load=[0]*3
-	task=list()
-	for i in range(np.random.randint(2,6)):	task.append(np.random.poisson(20))
-	text=''
-	d.append(route.create_text( 850,50,text='Task:',font=('Arial', 16)))
-	for i in range(len(task)):
-		d.append(route.create_text( 850,75+20*i,text='Subtask'+str(i+1)+' : '+str(task[i])+'%CPU',font=('Arial', 12)))
-	task.sort()
-	index=0
-	for e in task:
+	load=[0]*6
+	total_task=list()
+	for n_u in range(np.random.randint(2,6)):
+		task=list()
+		for i in range(np.random.randint(2,6)):	
+			cycle=np.random.poisson(15)
+			task.append(cycle)
+			total_task.append(cycle)	
+		d.append(route.create_text(150*(n_u+1),450,text='User'+str(n_u+1)+':',font=('Arial', 16)))
+		for i in range(len(task)):
+			d.append(route.create_text( 150*(n_u+1),475+20*i,text='Subtask'+str(i+1)+' : '+str(task[i])+'%CPU',font=('Arial', 12)))
+		task.sort()
+		index=0
+	for e in total_task:
 		while 1:
-			if index>2:
+			if index>5:
 				index=0
 			if load[index]<100:
 				load[index]+=e
 				index+=1
 				break
 			else:	index+=1
-	def node1():	r=requests.request('POST','http://140.112.20.183:11111', headers=headers, data=json.dumps({'data':task[0]}))
-	#def node2():	r=requests.request('POST','http://192.168.43.2:11111', headers=headers, data=json.dumps({'data':task[1]}))
+	#print(load)
+	def node1():	r=requests.request('POST','http://192.168.8.101:11111', headers=headers, data=json.dumps({'data':load[0]}))
+	def node2():	r=requests.request('POST','http://192.168.8.126:11111', headers=headers, data=json.dumps({'data':load[1]}))
+	def node2():	r=requests.request('POST','http://192.168.8.161:11111', headers=headers, data=json.dumps({'data':load[2]}))
+	def node2():	r=requests.request('POST','http://192.168.8.177:11111', headers=headers, data=json.dumps({'data':load[3]}))
+	def node2():	r=requests.request('POST','http://192.168.8.165:11111', headers=headers, data=json.dumps({'data':load[4]}))
+	def node2():	r=requests.request('POST','http://192.168.8.110:11111', headers=headers, data=json.dumps({'data':load[5]}))
 	Thread(target=node1).start()
-	#Thread(target=node2).start()
+	Thread(target=node2).start()
+	Thread(target=node3).start()
+	Thread(target=node4).start()
+	Thread(target=node5).start()
+	Thread(target=node6).start()
 
 d=list()
 value=[0]*4
--- cpu.py
+++ cpu.py
@@ -3,12 +3,11 @@ from threading import Thread
 import requests
 import json
 import os
-import numpy as np
 import time
 headers = {'Content-Type': 'application/json'}
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/')
 def sockeeet():
 	req=request.get_json()
 	print(req)
@@ -19,10 +18,10 @@ def sockeeet():
 		with open("output.txt") as f:
 			cpu_data={'instance':0,'data':f.read()[-56:-51]}
 			print(cpu_data)
-			r=requests.request('POST','http://140.112.77.188:5000', headers=headers, data=json.dumps(cpu_data))
+			r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps(cpu_data))
 			time.sleep(1)
 		i+=1
-	r=requests.request('POST','http://140.112.77.188:5000', headers=headers, data=json.dumps({'instance':3,'data':0}))
+	r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps({'instance':3,'data':0}))
 if __name__ == '__main__':
 	def writing():	os.system("sar 1 >output.txt")
 	Thread(target=writing).start()
