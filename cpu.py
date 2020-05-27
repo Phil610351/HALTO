eae3f99901dd34c2969ca854fb6a3b466ee3acd2
sSplendidSs <smyonlys@gmail.com>
2020-05-28 03:24:27 +0800

    123456

--- cpu.py
+++ cpu.py
@@ -11,19 +11,21 @@ app = Flask(__name__)
 def sockeeet():
 	req=request.get_json()
 	print(req)
+	def writing():	os.system("sar 1 >output.txt")
+	Thread(target=writing).start()	
 	def computing():	os.system("stress-ng -c 2 -l "+str(req['data'])+' --timeout 15')
 	Thread(target=computing).start()
 	i=0
 	count=0
 	while i<15:
-		def writing():	os.system("sar 1 >output.txt")
-		Thread(target=writing).start()
 		with open("output.txt") as f:
-			read=f.read()[-56:-51]
-			cpu_data={'instance':0,'data':read}
-			print(cpu_data)
-			count+=int(read)
-			r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps(cpu_data))
+			try:		
+				read=f.read()[-56:-51]
+				cpu_data={'instance':0,'data':read}
+				print(cpu_data)
+				count+=int(read)
+				r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps(cpu_data))
+			except:	pass
 			time.sleep(1)
 		i+=1
 	r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps({'instance':0,'data':count/15}))
