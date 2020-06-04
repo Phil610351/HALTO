507b60e01ce58e925333cbb3435b181631d8cbb7
Sublimenu <ssmyonlyss@egmail.com>
2020-06-04 21:50:48 +0800

    12345

--- VNE.py
+++ VNE.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from threading import Thread
 from tkinter import *
+from math import log
 import json
 import requests
 import numpy as np
@@ -36,11 +37,10 @@ def draw():
 	global c
 	global d
 	root = Tk()
-	route = Canvas(root,width=1200, height=700)
+	route = Canvas(root,width=1200, height=1000)
 	route.pack()
-	route.bind( "<Button-1>", gen_task)	
+	route.bind( "<Button-1>", gen_traffic)	
 
-	#route.create_text(100,50,text='State: ',font=('Arial', 20))
 	d.append(route.create_text(150,50,text='Initial',fill='brown',font=('Arial', 24)))
 
 	route.create_oval( 425, 50, 525, 150, width = 3 )
@@ -60,77 +60,162 @@ def draw():
 
 	c.append(route.create_text(100,300,text='0',font=('Arial', 16)))
 	route.create_oval( 50, 250, 150, 350, width = 3 )
-	#route.create_text(100,370,text='Node1',font=('Arial', 16))
 	c.append(route.create_text(250,300,text='0',font=('Arial', 16)))
 	route.create_oval( 200, 250, 300, 350, width = 3 )
-	#route.create_text(250,370,text='Node2',font=('Arial', 16))
 	c.append(route.create_text(400,300,text='0',font=('Arial', 16)))
 	route.create_oval( 350, 250, 450, 350, width = 3 )
-	#route.create_text(400,370,text='Node3',font=('Arial', 16))
 	c.append(route.create_text(550,300,text='0',font=('Arial', 16)))
 	route.create_oval( 500, 250, 600, 350, width = 3 )
-	#route.create_text(550,370,text='Node4',font=('Arial', 16))
 	c.append(route.create_text(700,300,text='0',font=('Arial', 16)))
 	route.create_oval( 650, 250, 750, 350, width = 3 )
-	#route.create_text(700,370,text='Node5',font=('Arial', 16))
 	c.append(route.create_text(850,300,text='0',font=('Arial', 16)))
 	route.create_oval( 800, 250, 900, 350, width = 3 )
-	#route.create_text(850,370,text='Node6',font=('Arial', 16))
 	
-	route.create_line(950, 0, 950, 700, width= 5 )
-	#route.create_line(0, 400, 950, 400, width= 5 )
+	route.create_line(950, 0, 950, 1000, width= 5 )
 	root.mainloop()
 
-def gen_task(event):
-	global d
-	for e in d:
-		route.delete(e)
-	d=list()
+def gen_traffic(event):
+	def sine():
+		traffic=2
+		incre=1	
+		while 1:
+			start=time.time()
+			global d
+			for e in d:	route.delete(e)
+			d=list()
+
+			total_task=list()
+			if traffic>3 and traffic<7:	d.append(route.create_text(150,50,text='Hybrid',fill='green',font=('Arial', 24)))
+			elif traffic>=7:	d.append(route.create_text(150,50,text='Online',fill='red',font=('Arial', 24)))
+			else:	d.append(route.create_text(150,50,text='Offline',fill='blue',font=('Arial', 24)))
+			anchor=0
+			for n_u in range(traffic):
+				task=list()
+				for i in range(np.random.randint(2,6)):	task.append(np.random.poisson(20))
+				total_task.append(task)
+				d.append(route.create_text(1000,25+anchor,text='User'+str(n_u+1)+':',font=('Arial', 16)))
+				anchor+=20
+				for i in range(len(task)):
+					d.append(route.create_text(1050,25+anchor,text='Subtask'+str(i+1)+' : '+str(task[i])+'%CPU',font=('Arial', 12)))
+					anchor+=20
+				anchor+=10
+			index=0
+			bias=[0]*6
+			load=[0]*6
+			decision=genetic(total_task)
+			for i in range(len(total_task)):
+				for j in range(len(total_task[i])):
+						load[int(decision[index])]+=total_task[i][j]
+						d.append(route.create_text(position[int(decision[index])][0],370+bias[int(decision[index])],text='('+str(i)+','+str(j)+')',font=('Arial', 16)))
+						bias[int(decision[index])]+=20
+						index+=1
+			print(decision)
+			print(load)
+			def node1():	r=requests.request('POST','http://192.168.8.126:11111', headers=headers, data=json.dumps({'data':load[0]}))
+			def node2():	r=requests.request('POST','http://192.168.8.161:11111', headers=headers, data=json.dumps({'data':load[1]}))
+			def node3():	r=requests.request('POST','http://192.168.8.177:11111', headers=headers, data=json.dumps({'data':load[2]}))
+			def node4():	r=requests.request('POST','http://192.168.8.134:11111', headers=headers, data=json.dumps({'data':load[3]}))
+			def node5():	r=requests.request('POST','http://192.168.8.165:11111', headers=headers, data=json.dumps({'data':load[4]}))
+			def node6():	r=requests.request('POST','http://192.168.8.110:11111', headers=headers, data=json.dumps({'data':load[5]}))
+			Thread(target=node1).start()
+			Thread(target=node2).start()
+			Thread(target=node3).start()
+			Thread(target=node4).start()
+			Thread(target=node5).start()
+			Thread(target=node6).start()
+			while time.time()-start<3:	pass
+			traffic+=incre
+			if traffic>7:
+				incre=-1
+			if traffic<3:
+				incre=1
+	Thread(target=sine).start()
+
+def cal_reward(state, decision):
 	load=[0]*6
-	total_task=list()
-	traffic=np.random.randint(2,7)
-	if traffic>2 and traffic<5:	d.append(route.create_text(150,50,text='Hybrid',fill='green',font=('Arial', 24)))
-	elif traffic>5:	d.append(route.create_text(150,50,text='Online',fill='red',font=('Arial', 24)))
-	else:	d.append(route.create_text(150,50,text='Offline',fill='blue',font=('Arial', 24)))
-	anchor=0
-	for n_u in range(traffic):
-		task=list()
-		for i in range(np.random.randint(2,6)):	
-			task.append(np.random.poisson(15))
-		total_task.append(task)
-		d.append(route.create_text(1000,25+anchor,text='User'+str(n_u+1)+':',font=('Arial', 16)))
-		anchor+=20
-		for i in range(len(task)):
-			d.append(route.create_text(1050,25+anchor,text='Subtask'+str(i+1)+' : '+str(task[i])+'%CPU',font=('Arial', 12)))
-			anchor+=20
-		anchor+=20
-		task.sort()
-		index=0
-	bias=[0]*6
-	for i in range(len(total_task)):
-		for j in range(len(total_task[i])):	
-			while 1:
-				if index>5:index=0
-				if load[index]<100:
-					load[index]+=total_task[i][j]
-					d.append(route.create_text(position[index][0],370+bias[index],text='('+str(i)+','+str(j)+')',font=('Arial', 16)))
-					bias[index]+=20
-					index+=1
-					break
-				else:	index+=1
-	#print(load)
-	#def node1():	r=requests.request('POST','http://192.168.8.126:11111', headers=headers, data=json.dumps({'data':load[0]}))
-	#def node2():	r=requests.request('POST','http://192.168.8.161:11111', headers=headers, data=json.dumps({'data':load[1]}))
-	#def node3():	r=requests.request('POST','http://192.168.8.177:11111', headers=headers, data=json.dumps({'data':load[2]}))
-	#def node4():	r=requests.request('POST','http://192.168.8.134:11111', headers=headers, data=json.dumps({'data':load[3]}))
-	def node5():	r=requests.request('POST','http://192.168.8.165:11111', headers=headers, data=json.dumps({'data':load[4]}))
-	#def node6():	r=requests.request('POST','http://192.168.8.110:11111', headers=headers, data=json.dumps({'data':load[5]}))
-	#Thread(target=node1).start()
-	#Thread(target=node2).start()
-	#Thread(target=node3).start()
-	#Thread(target=node4).start()
-	Thread(target=node5).start()
-	#Thread(target=node6).start()
+	index=0
+	for i in range(len(state)):
+		for j in range(len(state[i])):
+			load[int(decision[index])]+=state[i][j]
+			index+=1
+	entropy=0
+	for e in load:
+		entropy-=e/1000*log(e/1000+0.1)
+	return entropy
+
+def genetic(state):
+	Maternal=dict()
+	def generate(size):
+		for a in range(size):
+			decision=''
+			for i in range(len(state)):	
+				for j in range(len(state[i])):
+					decision+=str(np.random.randint(6))
+			Maternal[decision]=cal_reward(state, decision)
+
+	def crossover(pairs, rank):
+		parent=set()
+		for e in range(2*pairs):
+			a=np.random.rand()
+			if a<rank[0][1]:	parent.add(rank[0][0])
+			else:
+				accu=rank[0][1]
+				for i in range(1, len(rank)):
+					if a>accu and a<accu+rank[i][1]:
+						parent.add(rank[i][0])
+						break
+					else:	accu+=rank[i][1]
+		while len(parent)>2:
+			a=parent.pop()
+			b=parent.pop()
+			start, end=np.random.randint(len(a)), np.random.randint(len(a))
+			if start>end:   start,end=end,start
+			aa,bb='',''
+			for j in range(start):
+				aa+=a[j]
+				bb+=b[j]
+			for j in range(start, end):
+				aa+=b[j]
+				bb+=a[j]
+			for j in range(end, len(a)):
+				aa+=a[j]
+				bb+=b[j]
+			if aa not in Maternal:  Maternal[aa]=cal_reward(state, aa)
+			if bb not in Maternal:  Maternal[bb]=cal_reward(state, bb)
+			
+			if np.random.rand()<0.06:
+				muta1, muta2='', ''
+				tar=np.random.randint(len(aa))
+				for i in range(len(aa)):
+					if i!=tar:
+						muta1+=aa[i]
+						muta2+=bb[i]
+					else:
+						muta1+=str(np.random.randint(6))
+						muta2+=str(np.random.randint(6))
+				if muta1 not in Maternal:  Maternal[muta1]=cal_reward(state, muta1)
+				if muta2 not in Maternal:  Maternal[muta2]=cal_reward(state, muta2)
+
+		if len(Maternal)>len(rank):
+			for i in range(1, len(Maternal)-len(rank)+1):
+				Maternal.pop(rank[-i][0])
+
+	generate(1000)
+	cou=0
+	st1=0
+	while 1:
+		total=0
+		Roulette=Maternal.copy()
+		for e in Roulette.values(): total+=e
+		for e in Roulette.keys():   Roulette[e]/=total
+		crossover(25, sorted(Roulette.items(), key=lambda kv: -kv[1]))
+		if st1==sorted(Maternal.items(), key=lambda kv: -kv[1])[0][1]:  cou+=1
+		else:
+			st1=sorted(Maternal.items(), key=lambda kv: -kv[1])[0][1]
+			cou=0
+		if cou>10:   break
+	decision=sorted(Maternal.items(), key=lambda kv: -kv[1])[0][0]
+	return decision
 
 if __name__ == '__main__':
 	Thread(target=draw).start()
--- cpu.py
+++ cpu.py
@@ -13,23 +13,23 @@ def sockeeet():
 	print(req)
 	def writing():	os.system("sar 1 >output.txt")
 	Thread(target=writing).start()	
-	def computing():	os.system("stress-ng -c 2 -l "+str(req['data'])+' --timeout 15')
+	def computing():	os.system("stress-ng -c 2 -l "+str(req['data'])+' --timeout 3')
 	Thread(target=computing).start()
 	i=0
 	MA=list()
-	while i<15:
+	while i<3:
 		with open("output.txt") as f:
 			try:		
 				read=f.read()[-56:-51]
 				MA.append(float(read))
 				cpu_data={'instance':0,'data':round(sum(MA)/2/len(MA),2)}
 				print(cpu_data,read)
-				r=requests.request('POST','http://192.168.8.155:5000', headers=headers, data=json.dumps(cpu_data))
+				r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps(cpu_data))
 				if len(MA)>4:
 					MA.pop(0)
 			except:	pass
 			time.sleep(1)
-	r=requests.request('POST','http://192.168.8.155:5000', headers=headers, data=json.dumps({'instance':7,'data':0}))
+	r=requests.request('POST','http://192.168.8.139:5000', headers=headers, data=json.dumps({'instance':7,'data':0}))
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0',port=11111)
